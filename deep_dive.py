f063b78558477e6a3e56943a8755c8edafe2e146
shriya666 <shriyarajurkar3@gmail.com>
2024-09-24 01:35:59 +0000

    happy lemon cake.

--- deep_dive.py
+++ deep_dive.py
@@ -1,1 +1,18 @@
+import random
+
+name="Shriya"
+age=13
+favcolor="majestic purple"
+likerain=True
+
+characteristics=[name, age, favcolor, likerain]
+print(characteristics)
+
+string="hello" + " world"
+print(string)
+Remainder=11572340%34934
+print(Remainder)
+amongus=124374920.49890735280900897
+amongus=((amongus/24)**6)-7
+print(amongus)
 
