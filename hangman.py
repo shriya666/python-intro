db3a915dea77ef0feea89f6bdae74dce7b04ba13
shriya666 <shriyarajurkar3@gmail.com>
2024-11-19 02:33:02 +0000

    platypus

--- hangman.py
+++ hangman.py
@@ -1,12 +1,133 @@
+
 import random
-words=[
-    "computer"
-    "cheese"
-    "mouse"
-    "house"
-    "chair"
-    "laptop"
-    "sigma"
+words1=[
+    "computer",
+    "cheese",
+    "mouse",
+    "chair",
+    "laptop",
+    "phone",
+    "ipad",
+    "monitor"
+
 ]
-letter1= input("Guess a letter ")
-if 
+words2=[
+    "cake",
+    "camera",
+    "canvas",
+]
+words3=[
+    "defenestrate",
+    "retaliate"
+    "representative"
+]
+level=1
+
+HANGMANPICS = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+
+if level == 1:
+    word= random.choice(words)
+    blanks= []
+    for i in word:
+        blanks.append("_")
+    errors= 0
+    while "_" in blanks:
+        print("".join(blanks))
+        guess= input("Guess a letter ")
+        if guess in word:
+            # blanks[word.index(guess)]=guess
+            for i in range (len (word)):
+                if word [i]==guess:
+                    blanks[i]=guess
+        else:
+            print("Try again ")
+            errors+=1
+        if errors>= len (HANGMANPICS):
+            print("You are a dissapointment to society. Good bye. ")
+            break
+        print(HANGMANPICS[errors])
+    if "_" not in blanks:
+        print("You did it!")
+        print(word)
+        print(errors)
+        level+=1
+        moving=True
+if level==2 and moving==True:
+   word= random.choice(words)
+    blanks= []
+    for i in word:
+        blanks.append("_")
+    errors= 0
+    while "_" in blanks:
+        print("".join(blanks))
+        guess= input("Guess a letter ")
+        if guess in word:
+            # blanks[word.index(guess)]=guess
+            for i in range (len (word)):
+                if word [i]==guess:
+                    blanks[i]=guess
+        else:
+            print("Try again ")
+            errors+=1
+        if errors>= len (HANGMANPICS):
+            print("You are a dissapointment to society. Good bye. ")
+            break
+        print(HANGMANPICS[errors])
+    if "_" not in blanks:
+        print("You did it!")
+        print(word)
+        print(errors)
+        level+=1
+        moving=True 
+
+        
+
