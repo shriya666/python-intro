d17ae4080a579d98e9d03b05e5f2960ede6c7e12
shriya666 <shriyarajurkar3@gmail.com>
2025-02-17 20:03:00 +0000

    functionsgit add .git add .

--- functions.py
+++ functions.py
@@ -33,3 +33,35 @@ def unique(lst):
             newlist.append(i)
     return newlist
 print(unique([1,2,2,3]))
+
+# #even numbers from a list
+# def even_numbers(nums):
+#     newlist1=[]
+#     # divisible= nums % 2
+#     for i in nums:
+#         if i % 2==0:
+#             newlist1.append(i)
+
+#     return newlist1
+#         # if divisible== 0:
+#         #     return nums
+# print(even_numbers([1, 2, 3, 4, 564, 8, 9]))
+
+    #even numbers are divisible by two so you have to check if the number is divisible by 2
+    # for i in nums:
+    #     if i/2:
+    #         return 
+
+def is_palindrome(str):
+    left= 0
+    right= len(str) -1
+    while right >= left:
+        if not str[left]== str[right]:
+            return False
+        left += 1
+        right -=1
+    return True
+
+print(is_palindrome("racecar"))
+
+
