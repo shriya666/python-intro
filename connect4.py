2679d259bdf420a4ee5ded188f88e8f1fb43f18d
shriya666 <shriyarajurkar3@gmail.com>
2025-09-09 01:31:04 +0000

    in a red row

--- connect4.py
+++ connect4.py
@@ -12,25 +12,74 @@ board= [
 
 
 def find_bottom(col):
-    if board[-1][col]==0:
+    if board[4][col]==0:
         return 4
+    elif board[3][col]==0:
+        return 3
+    elif board [2][col]==0:
+        return 2
+    elif board[1][col]==0:
+        return 1
+    elif board[0][col]==0:
+        return 0
+    else:
+        return False
 
-column= int(input ("Which column? 1, 2, 3, 4, or 5? "))
+def other_move():
+    move= int(input ("Which column? 1, 2, 3, 4, or 5? "))
+    r = find_bottom(move)
+    board[r][move]= "🔴"
+    return
+def you_won(c):
+    in_a_row=0
+    for row in board:
+        if row[c]== "🟡":
+            in_a_row+=1
+            if in_a_row==4:
+                print("Yellow won!!!! ")
+                game==False
+                break
+        if row[c]== "🔴":
+            in_a_row= 0
+def red_won(d):
+    in_a_red_column=0
+    for row in board:
+        if row[d]== "🔴":
+            in_a_red_column+=1
+            if in_a_red_column==4:
+                print("Red won!!!!!")
+                game==False
+                break
+            if row[d]=="🟡":
+                in_a_red_column=0
+game= True 
+while game==True: 
+    column= int(input ("Which column? 1, 2, 3, 4, or 5? "))
+    empty_row = find_bottom(column-1)
+    if empty_row== False:
+         print("Out of Bounds. Pick again. ")
+    board[empty_row][column-1]="🟡"
+    for i in board:
+        print(i)
+    you_won(column-1)
+    move= int(input ("Which column? 1, 2, 3, 4, or 5? "))
+    r = find_bottom(move-1)
+    board[r][move-1]= "🔴"
+    for i in board:
+        print(i)
+    red_won(move-1)
 
-empty_row = find_bottom(column)
+def horizontal_win():
+    in_a_yellow_row=0
+    if board[]
 
 
-board[empty_row][column]="y"
-for i in board:
-    print(i)
 
 
-def computers_move():
-    move=random.randint(0, 4)
-    find_bottom(move)
-    return 
 
-computers_move()
+
+    
+
 
 
 
