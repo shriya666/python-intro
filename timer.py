4b0011c8b468db495a850424f649c2872a52ac35
shriya666 <shriyarajurkar3@gmail.com>
2025-10-14 01:31:55 +0000

    speech timer

---
+++ timer.py
@@ -0,0 +1,52 @@
+import termios
+import time
+choice= input("Normal timer, speech timer, or a stopwatch? ")
+if choice=="Normal Timer":
+    def stopping_time():
+        ask= int(input("How long do you want the timer to run? (in secs) "))
+        while ask>0:
+            time.sleep(1)
+            print(ask-1)
+            ask-=1
+        print("Time's up. ")
+
+    stopping_time()
+
+elif choice=="speech timer":
+    def speech_timer():
+        print("Starting Now ")
+        counter=60
+        while counter > -30:
+            time.sleep(1)
+            print(counter-1)
+            counter-=1
+            if counter==0:
+                print("Time's up. ")
+        print("Stop talking. ")
+
+    speech_timer()
+
+# def stopwatch():
+#     print("Type stop to stop. ")
+#     start= input("Type start to start ")
+#     counter= 0
+#     new = termios.tcgetattr(0)
+#     old = termios.tcgetattr(0)
+    
+    
+#     new[3] = ~(termios.ICANON)
+#     new[6][termios.VMIN] = 1
+#     new[6][termios.VTIME] = 0
+#     termios.tcsetattr(0, termios.TCSANOW, new) 
+
+#     while start!= "stop":
+#         input("> ")
+#         print(counter+1) 
+#         counter+=1
+
+# stopwatch()
+
+
+
+
+
